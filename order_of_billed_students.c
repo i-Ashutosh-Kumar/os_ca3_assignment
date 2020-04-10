bbd26eda28040f47f44933e5bdbc27cc2bf2acf4
Ashutosh Kumar <ashukumar88098@gmail.com>
2020-04-10 13:08:42 +0530

    Update order_of_billed_students.c

--- order_of_billed_students.c
+++ order_of_billed_students.c
@@ -4,54 +4,55 @@
 
 #include <stdio.h> 
 
-	int i,t,j,n;           //variable declaration
-	int B_T[10],p_no[10],bill[10]; //B_T=brust time ,p_no processno
+	int i,t,j,n;           //declaration of variables
 	float cost;
+	int B_T[10],p_no[10],bill[10]; //B_T=brust time ,p_no= process number
 	
- void billfn()              //bill generation
+	
+ void billfn()              //generation of bill
     {		  
-      printf( "STUDENT_NO\t\tTOTAL GIFTS PURCHASED\t\tBILL\n\n"); 
+      printf( "STUDENT_ORDER\t\tNO. OF GIFTS PURCHASED\t\tBILL AMOUNT\n\n"); 
 
 	    for (i = 0; i < n; i++) 
 		   { 
-	           	printf("STUDENT NO. = %d \t\t" ,p_no[i]);
+	           	printf("STUDENT Number. = %d \t\t" ,p_no[i]);
 		        printf("%d \t\t\t",B_T[i]);
-		        bill[i]= cost * B_T[i];           //calculating cost 
+		        bill[i]= cost * B_T[i];           //calculation of cost 
 	         	printf("%d \t\t\t",bill[i]); 
 	            printf("\n");
 	       }
     }
 
-void get() //list of gifts taken by students  (entered value by user)
+void get() //No. of gifts taken by students and entered by user
    {
 
      printf("\n\n");
-     printf("******************ENTERED DATA*****************\n\n");
-	 printf( "\nSTUDENT NO.\t\tNO.OF GIFTS PURCHASED \n"); 
+     printf("---------------NUMBER OF GIFTS---------------\n\n");
+	 printf( "\nSTUDENT Num.\t\tNumber OF GIFTS PURCHASED \n"); 
 
 	    for (i = 0; i < n; i++)  
 	      { 
-		      printf("STUDENT NO. = %d \t\t" ,p_no[i]);   //process no
+		      printf("STUDENT Num. = %d \t\t" ,p_no[i]);   //process no
 		      printf("%d \t\t\t",B_T[i]);                  //brust time
 	          printf("\n\n");
 	      } 
 	}
 
-void sorting()    //arrangement of students according to no of gifts chosen 
+void sorting()    //order of billing for students on the basis of number of gifts purchased
    { 
    
-	printf("**********ORDER OF BILLED STUDENTS************* \n\n");
+	printf("----------BILLING ORDER OF STUDENTS---------- \n\n");
 	    for(i=0;i<n;i++)
           {
     	      for(j=i+1;j<n;++j)
 			     {
-		            if(B_T[i]<B_T[j])                               //student with max no of gifts is given priority
+		            if(B_T[i]<B_T[j])              //priority given on the basis of number of gifts 
 					{
-		                t=B_T[i];                        //swapping the values 
+		                t=B_T[i];                        //values swapped
                    	    B_T[i]=B_T[j];
                         B_T[j]=t;
-                        t=p_no[i];           //t is temporary variable
-                        p_no[i]=p_no[j];                    //swapping the process no's
+                        t=p_no[i];           //temporary variable
+                        p_no[i]=p_no[j];                    //process no's swapping
                         p_no[j]=t;
                     }
                  }
@@ -61,17 +62,17 @@ void sorting()    //arrangement of students according to no of gifts chosen
 int main() 
 { 
 
-	 printf("ENTER THE NO OF STUDENTS WHO PURCHASE GIFT :- ");  // process number initialization
+	 printf("TOTAL NUMBER OF STUDENTS WHO PURCHASED GIFTS = ");  // process number initialization
      scanf("%d",&n);
-     printf("ENTER THE COST OF ONE GIFT :- ");
+     printf("COST OF EACH GIFT = ");
      scanf("%f",&cost);
-     printf("***************ENTER THE NUMBER OF GIFTS CORRESPONDING TO EACH STUDENT *************");
+     printf("---------------PLEASE ENTER THE NUMBER OF GIFTS FOR EACH STUDENT--------------");
         for(i=0;i<n;i++)
           {
               printf("\nSTUDENT_NO[%d] \n",i+1);
-              printf("ENTER THE NUMBER OF GIFTS PURCHASED :");//user input data
+              printf("NUMBER OF GIFTS PURCHASED :");//input by user
               scanf("%d",&B_T[i]);
-              p_no[i]=i+1;           //its process number 
+              p_no[i]=i+1;           //process number 
           }
 
   
